919e398f0af9f6ebf9619d9087cb271d8cf53ef6
Ridam Garg <ridamgarg8979@gmail.com>
2020-09-13 23:03:55 +0530

    Done custom_order

--- custom_order.java
+++ custom_order.java
@@ -1,67 +1,101 @@
 // Code for Beginning Java's Cake & Cupcake Shop Tutorial
 
+import java.io.PrintStream;
 import java.util.Scanner;  // Needed for the Scanner class to read input
 
 public class custom_order {
 
-    // STEP 1 PRINTING HELLO WORLD TO CONSOLE
-    public static void main(String[] args) {
-        
-      System.out.println("Hello World!"); // print Hello World to console
-        
-    // TEST CODE
-    
-    // STEP 2 CREATE A SCANNER OBEJCT, DECLARE VARAIBLES, & PRINT STATEMENTS
-	  
+    private static PrintStream printf;
 
+	// STEP 1 PRINTING HELLO WORLD TO CONSOLE
+    public static void main(final String[] args) {
+
+      System.out.println("Hello World!"); // print Hello World to console
+
+      // TEST CODE
+
+      // STEP 2 CREATE A SCANNER OBEJCT, DECLARE VARAIBLES, & PRINT STATEMENTS
+      final Scanner scn = new Scanner(System.in);
+
+      String firstName; // User's first name.
+      String itemOrder; // Item ordered.
+      String frostingType; // Frosting ordered
+      String fillingType;// Filling ordered
+      String toppings; // Toppings ordered
+      // String input; //user input
+
+  final double cost = 15.00; // Cost of cake and cupcakes
+  final double TAX_RATE = 0.08; //Sales tax rate
+  double tax; //Amount of tax
 	 
     // Introduce shop and prompt user to input first name
-      
+    System.out.println("Welcome to Harry's Cake & Cupcake Shop");
+    System.out.println("We make custom cakes with our secret cake batter!");
 
 	  
 	  
     // TEST CODE
-    
+     //On this test everything was nice
+
     // STEP 3 INPUT YOUR NAME AND PREPARE TO VIEW MENU
-   
+    System.out.print("What is your good name Sir? ");
+    firstName = scn.nextLine();
+
+    System.out.print(firstName + ", Here is the menu Sir.");
+    System.out.print("\n");
+  
 	  
       
     // TEST CODE     
     
     // STEP 4 DISPLAY MENU
-
+    System.out.println("_______________________________________________");      
+    System.out.println("        MENU         QUANTITY    BASE COST  ");
+    System.out.println("_______________________________________________"); 
+    System.out.println("        Cake            1            $15     ");
+    System.out.println("   Set of Cupcakes      6            $15     ");
+    System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
+    System.out.println("Frostings (vanilla, chocolate, strawberry, coco)");
+    System.out.println("Fillings (mocha, mint, lemon, caramel, vanilla)");
+    System.out.println("Toppings (sprinkles, cinnamon, cocoa, nuts)");
+    System.out.println("_______________________________________________");
 	
       
 
     // TEST CODE     
-    
-    // STEP 5 PROMPT USER TO ORDER
+    //all works
 
+    // STEP 5 PROMPT USER TO ORDER
+    
+    System.out.println("Do you want CUPCAKES or a CAKE");
+    itemOrder = scn.nextLine();
   
-	  
-	  
-	  
       
     // TEST CODE
       
     // STEP 6 PROMPT USER TO CHOOSE FROSTING
 
-   
-	 
-	 
-	 
+    System.out.println("What type of FROSTING do you want?");
+    System.out.println("Vanilla, Chocolate, Strawberry or Coco");
+    frostingType = scn.nextLine();
       
     //TEST CODE
       
     // STEP 7 PROMPT USER TO CHOOSE FILLING
 
-
+    System.out.println("What type of FILLING do you want? ");
+    System.out.println("Mocha, Mint, Lemon, Caramel or Raspberry");
+    fillingType = scn.nextLine();
 	  
 	  
       
     // TEST CODE
       
     // STEP 8 PROMPT USER TO CHOOSE TOPPINGS
+    
+    System.out.println("What type of TOPPINGS do you want? ");
+    System.out.println("Sprinkles, Cinnamon, Cocoa, Nuts");
+    toppings = scn.nextLine();
 
 
 	
@@ -71,7 +105,14 @@ public class custom_order {
       
     // STEP 9 DISPLAY ORDER CONFIRMATION
 
-
+    System.out.println();
+    System.out.println(firstName + " , your order is as follows: ");
+    System.out.println("_____________________________________________");
+    System.out.println("Item Ordered: " + itemOrder);
+    System.out.println("Frosting: " + frostingType);
+    System.out.println("Filling: " + fillingType);
+    System.out.println("Toppings: " + toppings);
+    System.out.println("_____________________________________________");
       
 	  
 	  
@@ -80,6 +121,12 @@ public class custom_order {
     // TEST CODE
       
     // STEP 10 DISPLAY COST AND SALES TAX
-  
-    }   
+    System.out.printf("The cost of your order is: $%.2f\n", cost);
+    tax = cost * TAX_RATE;
+    System.out.printf("The tax is: $%.2f\n", tax);
+    System.out.printf("The Bill is: $%.2f\n", (tax + cost));
+   System.out.printf("It was nice seeing you"+" "+firstName);
+   System.out.println();
+   System.out.print("Good Bye!!");
+    }
 }
